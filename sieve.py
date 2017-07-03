24e51c4a0030c35efcb4ff024e08217f69d7626a
pandeyshrey33 <pandeyshrey33@gmail.com>
2017-07-03 17:58:39 +0530

    Create sieve.py

---
+++ sieve.py
@@ -0,0 +1,11 @@
+def sieve(limit):
+    a = [True] * limit                          # Initialize the primality list
+    a[0] = a[1] = False
+
+    for (i, isprime) in enumerate(a):
+        if isprime:
+            yield i
+            for n in xrange(i*i, limit, i):     # Mark factors non-prime
+                a[n] = False
+
+print sieve(1000)
